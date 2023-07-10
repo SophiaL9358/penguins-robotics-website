f1181a915f685b58792e8d2125b9694803551e2b
soapity <mrows9356@gmail.com>
2023-07-09 22:48:43 -0400

    changing names to pigeon-bots

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: "https://sophial9358.github.io/penguins-robotics-website/",
+  base: "https://sophial9358.github.io/pigeon-bots-website/",
 })
